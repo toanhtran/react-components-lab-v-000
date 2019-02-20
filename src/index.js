3654385d733f863981e679a02fbc3146eec5962d
ToAnh Tran <toanh.t.tran@gmail.com>
2019-02-20 03:22:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -77,16 +77,3 @@ export class App extends React.Component {
   }
 }
 ReactDOM.render(<App />, document.getElementById('root'))
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
